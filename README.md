b879af4e3be2c2cf33ce5ad699d015583c74f508
GonzaVice <givicente@miuandes.cl>
2024-08-06 19:14:19 -0400

    MongoDB database connected

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # assignment1-g7
+
+## Notas:
+
+### Descargar express, mongoose y dotenv
+
+`npm i express mongoose dotenv`
+
+### Crear .env para las credenciales del MongoDB
+
+```
+MONGODB_URI=mongodb://localhost:27017/libros_criticas_db
+PORT=3000
+```
--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
 const app = express();
-const port = 3000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server corriendo en el puerto ${PORT}`);
 });
